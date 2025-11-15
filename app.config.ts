92d4c942c9ced458d7967c8a70c60762d4daf5e3
Verninii <lorenzzo.vernini@gmail.com>
2025-11-14 21:05:33 -0300

    gerado apk

--- app.config.ts
+++ app.config.ts
@@ -1,16 +1,15 @@
-// frontend/app.config.ts
-import 'dotenv/config';
-
 export default {
-  expo: {
-    name: 'SeuApp',
-    slug: 'seuapp',
-    scheme: 'seuapp', // para deep links se precisar
-    extra: {
-      // Estes valores ficam acessíveis em runtime via expo-constants
-      API_URL: process.env.API_URL,
-      API_TOKEN: process.env.API_TOKEN,
-      DAILY_KCAL_GOAL: Number(process.env.DAILY_KCAL_GOAL || 2000),
-    },
+  name: "SeuApp",
+  slug: "seuapp",
+  scheme: "seuapp",
+
+  extra: {
+    API_URL: process.env.API_URL,
+    API_TOKEN: process.env.API_TOKEN,
+    DAILY_KCAL_GOAL: Number(process.env.DAILY_KCAL_GOAL || 2000),
+  },
+
+  android: {
+    package: "com.anonymous.seuapp", // escolha o ID do seu app
   },
 };
